76753d29286163965dc8ffac0a45c28df8f7d5b8
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2020-09-23 21:41:22 +0100

    fixed error before build

--- src/components/About.js
+++ src/components/About.js
@@ -14,13 +14,13 @@ function About() {
         <div className="column">
           <div className="columnTwo">
             <p>
-              I'm a junior full stack developer who has just graduated from <a target="_blank" href="https://wearecodenation.com/" className="aboutLinks"> Code Nation</a> - one of the UK's leading coding schools. I was awarded the <a target="_blank" href="https://digitalher.co.uk/digital-her-scholarship/" className="aboutLinks">Digital Her</a> scholarship, which I am very proud of.
+              I'm a junior full stack developer who has just graduated from <a target="_blank" rel="noopener noreferrer" href="https://wearecodenation.com/" className="aboutLinks"> Code Nation</a> - one of the UK's leading coding schools. I was awarded the <a target="_blank" rel="noopener noreferrer" href="https://digitalher.co.uk/digital-her-scholarship/" className="aboutLinks">Digital Her</a> scholarship, which I am very proud of.
             </p>
             <p>
               At the beginning of the COVID-19 lockdown, I lost my job in HR. Instead of letting this get me down, I decided this was the perfect opportunity for a career change.
             </p>
             <p>
-              I had recently taken up music production as a hobby, live coding music on <a target="_blank" href="https://sonic-pi.net/" className="aboutLinks">Sonic Pi</a>, so I decided to put this skill to good use. I did some FreeCodeCamp courses and completed a three week course at Code Nation before starting the 12 week bootcamp.
+              I had recently taken up music production as a hobby, live coding music on <a target="_blank" rel="noopener noreferrer" href="https://sonic-pi.net/" className="aboutLinks">Sonic Pi</a>, so I decided to put this skill to good use. I did some FreeCodeCamp courses and completed a three week course at Code Nation before starting the 12 week bootcamp.
             </p>
             <p>
               My degree in French and Spanish is helpful too - they do call Javascript a programming language, after all!
