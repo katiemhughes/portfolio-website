3fd7bb6439ba30ac3a52890938ad5511a58268fe
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2020-10-08 19:02:05 +0100

    added links to social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ function Footer() {
     return (
         <div className="footerContainer">
             <div className="socialIcons">
-                <SocialIcon network="twitter" bgColor="#00000" />
-                <SocialIcon network="github" bgColor="#00000" />
-                <SocialIcon network="linkedin" bgColor="#00000" />
-                <SocialIcon network="email" bgColor="#00000" />
+                <SocialIcon network="twitter" bgColor="#00000" url="https://twitter.com/katiemhughes_" />
+                <SocialIcon network="github" bgColor="#00000" url="https://github.com/katiemhughes" />
+                <SocialIcon network="linkedin" bgColor="#00000" url="http://www.linkedin.com/in/katiemhughes95" />
+                <SocialIcon network="email" bgColor="#00000" url="mailto:katie.m.hughes@hotmail.co.uk" />
                 {/* <SocialMediaIconsReact className="social-icon-link" aria-label="Github" borderColor="rgba(0,0,0,1)" borderWidth="5" borderStyle="solid" icon="github" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(0,0,0,1)" iconSize="9" roundness="50%" url="https://github.com/katiemhughes" size="40" />
                 <SocialMediaIconsReact className="social-icon-link" aria-label="LinkedIn" borderColor="rgba(0,0,0,0.4)" borderWidth="5" borderStyle="solid" icon="linkedin" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(0,123,182,1)" iconSize="5" roundness="50%" url="http://www.linkedin.com/in/katiemhughes95" size="40" />
                 <SocialMediaIconsReact className="social-icon-link" aria-label="Twitter"  borderColor="rgba(0,0,0,0.3)" borderWidth="5" borderStyle="solid" icon="twitter" iconColor="rgba(255,255,255,1)" backgroundColor="rgba(28,186,223,1)" iconSize="5" roundness="50%" url="https://twitter.com/katiemhughes_" size="39" />
