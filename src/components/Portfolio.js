8c4ce9e5f593bf5a5a7fcf2eb8759483a97dce9e
Katie Hughes <katie.m.hughes@hotmail.co.uk>
2021-12-28 14:14:05 +0000

    added radio website to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Portfolio.css";
-import {makeStyles} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import { Box, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
-import project1 from "../images/interbot.png";
-import project2 from "../images/todolist.png";
-import project3 from "../images/weatherapp2.png";
-import project4 from "../images/portfolio.png";
-import project5 from "../images/vacationdetective.png";
+import project1 from "../images/rainycityradio.png";
+import project2 from "../images/interbot.png";
+import project3 from "../images/todolist.png";
+import project4 from "../images/weatherapp2.png";
+import project5 from "../images/portfolio.png";
+import project6 from "../images/vacationdetective.png";
 
 const useStyles = makeStyles({
     mainContainer: {
@@ -34,7 +35,17 @@ const useStyles = makeStyles({
         paddingTop: "-7%",
         paddingBottom: "10%",
         paddingLeft: "6%"
-    }
+    },
+    radioCardContent: {
+        paddingTop: "15px",
+        paddingRight: "16px",
+        paddingBottom: "8px",
+        paddingLeft: "16px"
+    },
+    radioSiteButtonContainer: {
+        justifyContent: "center",
+        paddingTop: "0",
+    },
 });
 
 const Portfolio = () => {
@@ -43,11 +54,35 @@ const Portfolio = () => {
         <Box component="div" className={classes.mainContainer}>
             <Grid container justify="center">
                 {/* Project 1 */}
-                <Grid item xs={12} sm={8} md={6}>
+                <Grid item xs={12} sm={8} md={4}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea className={classes.radioWebsiteCTAContainer}>
+                            <CardMedia component="img" alt="Project 1" height="-100" image={project1}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Rainy City Radio website
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Website I built for a radio station that I co-founded. I used ReactJS for the frontend and I built the radio player using a Linux Server, with AzuraCast broadcasting software.
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                            <CardActions className={classes.radioSiteButtonContainer}>
+                                <Button target="_blank" href="https://github.com/katiemhughes/radio-website" size="small" color="primary">
+                                    Github source code
+                                </Button>
+                                <Button target="_blank" href="https://rainycityradio.live/" size="small" color="primary">
+                                    Live site
+                                </Button>
+                            </CardActions>
+                    </Card>
+                </Grid>
+                {/* Project 2 */}
+                <Grid item xs={12} sm={8} md={4}>
                     <Card className={classes.cardContainer}>
                         <CardActionArea>
-                            <CardMedia component="img" alt="Project 1" height="-100" image={project1}
-
+                            <CardMedia component="img" alt="Project 2" height="-100" image={project2}
                             />
                             <CardContent>
                                 <Typography gutterBottom variant="h5">
@@ -67,12 +102,12 @@ const Portfolio = () => {
                                 </Button>
                             </CardActions>
                     </Card>
-                    </Grid>
-                    {/* Project 2 */}
-                <Grid item xs={12} sm={8} md={6}>
+                </Grid>
+                    {/* Project 3 */}
+                <Grid item xs={12} sm={8} md={4}>
                     <Card className={classes.cardContainer}>
                         <CardActionArea>
-                            <CardMedia component="img" alt="Project 2" height="140" image={project2}
+                            <CardMedia component="img" alt="Project 3" height="140" image={project3}
 
                             />
                             <CardContent>
@@ -94,11 +129,11 @@ const Portfolio = () => {
                             </CardActions>
                     </Card>
                 </Grid>
-                 {/* Project 3 */}
-                 <Grid item xs={12} sm={8} md={6}>
+                 {/* Project 4 */}
+                 <Grid item xs={12} sm={8} md={4}>
                     <Card className={classes.cardContainer}>
                         <CardActionArea>
-                            <CardMedia component="img" alt="Project 3" height="140" image={project3}
+                            <CardMedia component="img" alt="Project 4" height="140" image={project4}
 
                             />
                             <CardContent>
@@ -120,11 +155,11 @@ const Portfolio = () => {
                             </CardActions>
                     </Card>
                 </Grid>
-                        {/* Project 4 */}
-                    <Grid item xs={12} sm={8} md={6}>
+                        {/* Project 5 */}
+                    <Grid item xs={12} sm={8} md={4}>
                     <Card className={classes.cardContainer}>
                         <CardActionArea>
-                            <CardMedia component="img" alt="Project 4" height="140" image={project4}
+                            <CardMedia component="img" alt="Project 5" height="140" image={project5}
 
                             />
                             <CardContent>
@@ -143,11 +178,11 @@ const Portfolio = () => {
                             </CardActions>
                     </Card>
                 </Grid>
-                    {/* Project 5 */}
-                <Grid item xs={12} sm={8} md={6}>
+                    {/* Project 6 */}
+                <Grid item xs={12} sm={8} md={4}>
                     <Card className={classes.cardContainer}>
                         <CardActionArea>
-                            <CardMedia component="img" alt="Project 5" height="140" image={project5}
+                            <CardMedia component="img" alt="Project 6" height="140" image={project6}
 
                             />
                             <CardContent>
